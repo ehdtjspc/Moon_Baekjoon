a163b9b36d6128be72d5e85303a958e30c2949de
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-09-22 17:42:04 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dongTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            int b = 2541 - 1998;
+            int age = int.Parse(a);
+
+            Console.WriteLine(age - b);
+        }
+    }
+}
