c1c3b270f53a84f8fb425dc6de895d437444cdb4
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-09-22 19:25:49 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dongTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");     //문자열로 "나타내고 싶으면 \"하면 됨 역슬레시는 \\두번해주기
+
+        }
+    }
+}
