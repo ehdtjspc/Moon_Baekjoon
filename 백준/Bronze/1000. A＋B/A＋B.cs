d41a8be27aeed4d814ae16fcf65aaf9afa8fa528
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-09-22 16:36:27 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dongTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split( );    //문자열 두개를 받아 Split를 통해 두개로 나눔
+
+            Console.WriteLine(int.Parse(a[0]) + int.Parse(a[1]));
+
+        }
+    }
+}
