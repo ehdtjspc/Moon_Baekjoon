4d39843ffad0854efe1cf8dd0c23a0fa2407885d
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-09-22 17:21:16 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dongTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();    //문자열 두개를 받아 Split를 통해 두개로 나눔
+
+            Console.WriteLine(a + "??!");
+        }
+    }
+}
