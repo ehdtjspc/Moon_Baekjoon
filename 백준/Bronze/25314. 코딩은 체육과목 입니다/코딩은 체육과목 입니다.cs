e35a522484d4e51486c5dd57d73bb1960db87bb8
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-09-30 12:33:55 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 52 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE
+{
+    internal class Program{
+    static void Main(string[] args){
+
+        
+        int a = int.Parse(Console.ReadLine());
+        int sum = a/4;
+        
+       
+        for(int i = 0; i<sum; i++)
+        {
+            Console.Write("long ");
+        }
+        Console.Write("int");
+       
+
+}
+    }
+        }
