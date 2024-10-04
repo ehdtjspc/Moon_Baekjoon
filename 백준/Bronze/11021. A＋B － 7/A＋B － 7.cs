69b9286c4007809bbdd3382048b6d4bd0176ad31
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2024-10-04 10:13:51 +0900

    [Bronze V] Title: A+B - 7, Time: 52 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE
+{
+    internal class Program{
+    static void Main(string[] args){
+
+        
+        int a = int.Parse(Console.ReadLine());
+       
+        StringBuilder aa = new StringBuilder();
+    
+        int num = 0;
+       
+        for(int i = 1; i<=a; i++)
+        {
+             string[] b = Console.ReadLine().Split(" ");
+             int b1 = int.Parse(b[0]);
+             int b2 = int.Parse(b[1]);
+             num = b1+b2;
+             aa.AppendLine($"Case #{i}: {num}");     // 문자열 뒤에 덧붙이는 역할임, 새로운 메모리가 할당되는게 아니라 기존메모리에 뒤에 덧붙임 가변의 공간이
+
+        }
+        Console.WriteLine(aa.ToString());
+       
+    
+}
+    }
+        }
