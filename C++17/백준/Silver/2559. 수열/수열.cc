764432fcccab5d4a242f1bc6fa0632b937beec67
ehdtjspc <109285423+ehdtjspc@users.noreply.github.com>
2025-10-08 23:57:08 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int N , K, temp, psum[100001] , ret = -100004;
+
+
+int main()
+{
+	cin >> N >> K;
+	for(int i = 1; i<=N; i++)
+	{
+		cin >> temp;
+		psum[i] = psum[i-1] + temp;
+	}
+	for(int i = K; i <= N; i++)
+	  {
+		ret = max(ret,psum[i]-psum[i-K]);
+	}
+	cout <<ret<<"\n";
+	return 0;
+}
